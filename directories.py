8d8f914979614bc4108262180d8db568f1728959
jgproode <j.roode@aventus.nl>
2022-06-01 09:35:41 +0200

    lijst vor bestanden

--- directories.py
+++ directories.py
@@ -3,6 +3,9 @@ dirPath = r"C:\tmp\Diego-"
 #alle bestanden, geen dirs.
 result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
 print(result)
+lijst=[]
+lijst.append(result)
 print("------------------------") 
 result = next(os.walk(dirPath))[2]
 print(result)
+print("de lijst is "+ str(lijst))
