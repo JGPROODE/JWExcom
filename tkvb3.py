2408e67c3bafe05b6035a48152f4da9602857755
jgproode <j.roode@aventus.nl>
2022-06-22 12:04:14 +0200

    gui toegevoegd

--- tkvb3.py
+++ tkvb3.py
@@ -3,10 +3,28 @@ from typing import Text
 
 scherm=tk.Tk()
 
-groet=tk.Label(text="hoi wereld")
-melding=tk.Label(text="MOOI weer vandaag.....de zon schijnt en er is geen regen")
+groet=tk.Label(
+    text="hoi wereld",
+    foreground="red",
+    background="black",
+    padx=50,
+    pady=100
+    )
+
+invoerveld1=tk.Entry(
+    bd =15
+)
+
+melding=tk.Label(text="MOOI weer vandaag.....de zon schijnt en er is geen regen",
+
+  foreground="green",
+    background="yellow",
+    padx=50,
+    pady=100
+)
 
 groet.pack()
+invoerveld1.pack()
 melding.pack()
 
 scherm.mainloop()
